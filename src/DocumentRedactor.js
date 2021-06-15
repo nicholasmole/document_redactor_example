c64f0b246eaba713b9969a17ea35105251e7398b
nicholasmole <nmole065@g.rwu.edu>
2021-06-15 11:51:01 -0400

    [feat] Add seperator to DocumentRedactor documentation
    Update the DocumentRedactor documentation with the seperator parameter.
    The seperator parameter is an optional parameter that an be used to
    determine how the keywords are split.

--- src/DocumentRedactor.js
+++ src/DocumentRedactor.js
@@ -3,11 +3,11 @@
  * Redact keywords from given document text
  * @param  {string} keywords - keywords that will be redacted from document
  * @param  {string} document - text that needs to be redacted
+ * @param  {string} seperator - key value that tells the module how to distiguish between keyword
  * @return {string} - modified document with keywords in document replaced by "XXXX"
- */ 
+ */
 module.exports = function DoumentRedactor(keywords = "", document = "", seperator = " ") {
   keywords = `\\b(${keywords.replace(new RegExp(seperator,"gi"), "|")})\\b`;
   document = document.replace(new RegExp(keywords,"gi"), "XXXX");
   return document;
 };
-// exports.default = DoumentRedactor;
