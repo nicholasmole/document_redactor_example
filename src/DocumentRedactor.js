9778fa8d8f8f70d82ee17cd256c6e887453e46b4
nicholasmole <nmole065@g.rwu.edu>
2021-06-17 23:42:39 -0400

    [feat] Redactor handles multiple seperator rules
    The redactor can now handle multiple seperator rules at once. These
    include commas, spaces, and phrases inside of quotes. In case of
    quotes within quotes the system will keep the inner quotes as there
    original character.
    
    The redactor will not replace commas or spaces within quotes, but
    all commas, spaces, and outer quotation marks will become pipes to
    allow the regex to redact all keywords and phrases no matter the
    seperation notation.
    
    The final regex uses both word and not word boundary in order to
    avoid substring matching, and to allow for quotes within quotes to
    match exactly on the document string.

--- src/DocumentRedactor.js
+++ src/DocumentRedactor.js
@@ -7,7 +7,24 @@
  * @return {string} - modified document with keywords in document replaced by "XXXX"
  */
 module.exports = function DoumentRedactor(keywords = "", document = "", seperator = " ") {
-  keywords = `\\b(${keywords.replace(new RegExp(seperator,"gi"), "|")})\\b`;
-  document = document.replace(new RegExp(keywords,"gi"), "XXXX");
+
+  const SINGLE_QUOTES_REPLACEMENT = "SINGLE_QUOTES_REPLACEMENT";
+  const DOUBLE_QUOTES_REPLACEMENT = "DOUBLE_QUOTES_REPLACEMENT";
+  const REGEX_GET_SINGLE_QUOTES = new RegExp(/\'(?=[^"]*"[^"]*(?:"[^"]*"[^"]*)*$)/,"gi");
+  const REGEX_GET_DOUBLE_QUOTES = new RegExp(/\"(?=[^']*'[^']*(?:'[^']*'[^']*)*$)/,"gi");
+  const REGEX_GET_SPACES_AND_COMMAS_OUTSIDE_OF_QUOTES = new RegExp(/(\s+|,)(?=(?:[^("|')]|("|')[^("|')]*("|'))*$)/, "gi");
+  const REGEX_ALL_QUOTES = new RegExp(/('|")(?:\|)|(?:\|*)('|")/,"g");
+  const REGEX_CHECK_FOR_LEADING_OR_ENDING_PIPES = new RegExp(/^\||(\|)$/,"g");
+
+  keywords = keywords.replace(REGEX_GET_SINGLE_QUOTES, SINGLE_QUOTES_REPLACEMENT)
+              .replace(REGEX_GET_DOUBLE_QUOTES, DOUBLE_QUOTES_REPLACEMENT)
+              .replace(REGEX_GET_SPACES_AND_COMMAS_OUTSIDE_OF_QUOTES, "|")
+              .replace(REGEX_ALL_QUOTES, "|")
+              .replace(new RegExp(SINGLE_QUOTES_REPLACEMENT,"gi"),"'")
+              .replace(new RegExp(DOUBLE_QUOTES_REPLACEMENT,"gi"),'"')
+              .trim()
+              .replace(new RegExp(REGEX_CHECK_FOR_LEADING_OR_ENDING_PIPES,"gi"),"");
+
+  document = document.replace(new RegExp(`\\B(${keywords})\\B|\\b(${keywords})\\b`,"gi"), "XXXX");
   return document;
 };
